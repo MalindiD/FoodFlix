e00b07a5aa0d76c206b03874fc54be6728d142d7
Malindi <malindidiss123@gmail.com>
2025-04-29 21:59:10 +0530

    conflicts

--- backend/restaurant-service/server.js
+++ backend/restaurant-service/server.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 });
 
 // const PORT = process.env.PORT || 80;
-const PORT = 80;
+const PORT = 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
